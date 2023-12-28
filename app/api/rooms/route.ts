b7e6399430508962bdffce1cc7cee6aaf0929690
Gusth Gomes <gusthgomesdarosa@gmail.com>
2023-12-27 22:26:19 -0300

    feat: create route

---
+++ app/api/rooms/route.ts
@@ -0,0 +1,23 @@
+import dbConnect from "@/backend/config/dbConnect";
+import { allRooms, newRoom } from "@/backend/controllers/roomControllers";
+import { createEdgeRouter } from "next-connect";
+import { NextRequest } from "next/server";
+
+interface RequestContext {
+    params: { id: string }
+}
+
+const route = createEdgeRouter<NextRequest, RequestContext>()
+
+dbConnect();
+
+route.get(allRooms);
+route.post(newRoom);
+
+export async function GET(request: NextRequest, ctx: RequestContext) {
+    return route.run(request, ctx);
+}
+
+export async function POST(request: NextRequest, ctx: RequestContext) {
+    return route.run(request, ctx);
+}
