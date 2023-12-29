3c13a8fe4a8fd452684ac98f974e41c177aca8bc
Gusth Gomes <gusthgomesdarosa@gmail.com>
2023-12-28 21:52:50 -0300

    chore: changed settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        DB_LOCAL_URI: 'mongodb://127.0.0.1:27017/bookit',
-        DB_URI : '',
-    }
-}
-
-module.exports = nextConfig
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      domains: [""],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      };
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
+  
