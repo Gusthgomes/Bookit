2ee2dde9b81cdea266ce014e3dc2bc8b3af9d958
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-01-11 21:22:32 -0300

    feat: inserting components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from './head'
+import Script from 'next/script'
+import { GlobalProvider } from './GlobalProvider'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +22,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head/>
+      <body className={inter.className}>
+        <GlobalProvider>
+          <Header/>
+          {children}
+          <Footer/>
+        </GlobalProvider>
+
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></Script>
+        <Script src="https://kit.fontawesome.com/e43ae2f1aa.js"></Script>
+      </body>
     </html>
   )
 }
