b82bf142f075a1586b6f08f83f7dedc769b38b6c
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-01-12 21:38:48 -0300

    feat: reading rooms properties

--- components/room/RoomItem.tsx
+++ components/room/RoomItem.tsx
@@ -1,30 +1,35 @@
 'use client';
+import { IRoom } from '@/backend/models/Room';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
-const RoomItem = () => {
+interface Props{
+    room: IRoom,
+}
 
-    // TODO: implement Star Rating function
-    
+const RoomItem = ({ room }: Props ) => {
+
     return (
         <div className="col-sm-12 col-md-6 col-lg-3 my-3 d-flex">
             <div className="card p-2 w-100">
-                <img
+                <Image
                 className="card-img-top mx-auto"
-                src="images/default_room_image.jpg"
-                alt=""
-                height="170"
-                width="100"
+                src={room?.images?.length > 0 ? room.images[0].url : '/images/default_room_image.jpg'}
+                alt={room?.name}
+                height={170}
+                width={100}
                 />
                 <div className="card-body d-flex flex-column">
                 <h5 className="card-title">
-                    <a href="/rooms/roomId">Room Name</a>
+                    <a href="/rooms/roomId">{room?.name}</a>
                 </h5>
                 <div className="mt-auto">
-                    <p className="card-text mt-2"><b>$100</b> / night</p>
+                    <p className="card-text mt-2"><b>${room?.pricePerNight}</b> / night</p>
                 </div>
                 <div>
-                <a className="btn view-btn mt-3 w-100" href="/rooms/roomId"
-                >View Details</a>
+                <Link className="btn view-btn mt-3 w-100" href={`/rooms/${room._id}`}
+                >View Details</Link>
                 </div>
                 </div>
             </div>
