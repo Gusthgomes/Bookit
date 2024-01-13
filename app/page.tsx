79a214195c57940aead9769002333a6573dada3a
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-01-12 21:37:01 -0300

    feat: fetch all rooms of api

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import Home from "@/components/Home"
+import Home from "@/components/Home";
+import Error from "./error";
 
-export default function HomePage() {
-  return <Home />
+export const revalidate = 0;
+
+const getRooms = async () => {
+  const res = await fetch(`${process.env.API_URL}/api/rooms`);
+  return res.json();
+}
+
+export default async function HomePage() {
+  const data = await getRooms();
+
+  if(data?.message){
+    return <Error error={ data }/>
+  }
+
+  return <Home data={ data }/>
 }
