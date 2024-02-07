02b12cb4004c2a098aebbb15f37110d3e6dcc6fe
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-07 16:11:43 -0300

    chore: change number res per page

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -8,7 +8,7 @@ import APIFilters from "../utils/apiFilters";
 // Get all rooms => /api/rooms
 export const allRooms = catchAsyncErrors(async (req: NextRequest) => {
 
-    const resPerPage: number = 9;
+    const resPerPage: number = 8;
 
     const { searchParams } = new URL(req.url);
 
