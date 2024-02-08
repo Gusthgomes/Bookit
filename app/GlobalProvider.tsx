356f17676dd56c1195f95b27f314c1d8ebf3ea44
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-08 16:50:21 -0300

    chore: involving the application with the provider

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -1,8 +1,17 @@
+'use client';
+
 import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
+import { Provider } from 'react-redux';
+import { store } from '@/redux/store';
 
 export function GlobalProvider( { children } : { children: React.ReactNode} ){
     return <>
         <Toaster />
-        { children }
+        <Provider store={ store }>
+            <SessionProvider>
+                { children }
+            </SessionProvider>
+        </Provider>
     </>
 }
