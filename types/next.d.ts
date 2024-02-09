ed76fdc401d6f2503204ef18b98732b3c7dd96f0
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-09 16:30:08 -0300

    chore: moving files to an new source

--- types/next.d.ts
+++ types/next.d.ts
@@ -1,7 +1,15 @@
+import { IUser } from "@/backend/models/User";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
+import { NextRequest } from "next/server";
 
 declare module '@reduxjs/toolkit/query/react' {
     interface FetchBaseQueryError {
         data?:any
+    };
+};
+
+declare module 'next/server' {
+    interface NextRequest {
+        user: IUser
     };
 };
