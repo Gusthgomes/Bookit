27ff73d2b0118516d32a63ee1352b62078feafc5
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-09 16:07:16 -0300

    feat: insert redux

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,13 +1,25 @@
 'use client';
 
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { signOut, useSession } from "next-auth/react";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { setIsAuthenticated, setUser } from "@/redux/features/userSlice";
 
 const Header = () => {
 
+  const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state) => state.auth);
+
   const { data } = useSession();
 
+  useEffect( () => {
+    if(data) {
+      dispatch(setUser(data.user));
+      dispatch(setIsAuthenticated(true));
+    }
+  }, [data]);
+  
   const logoutHandler = () => {
     signOut();
   }
@@ -28,7 +40,7 @@ const Header = () => {
         </div>
 
         <div className="col-6 col-lg-3 mt-3 mt-md-0 text-end">
-          { data?.user ? (
+          { user ? (
             <div className="ml-4 dropdown d-line">
               <button
                 className="btn dropdown-toggle"
@@ -39,14 +51,14 @@ const Header = () => {
               >
                 <figure className="avatar avatar-nav">
                   <img
-                    src={ data?.user?.avatar ? data?.user?.avatar?.url : "/images/default_avatar.jpg" }
+                    src={ user?.avatar ? user?.avatar?.url : "/images/default_avatar.jpg" }
                     alt="Avatar"
                     className="rounded-circle placeholder-glow"
                     height="50"
                     width="50"
                   />
                 </figure>
-                <span className="placeholder-glow ps-1"> { data?.user?.name }</span>
+                <span className="placeholder-glow ps-1"> { user?.name }</span>
               </button>
 
               <div
