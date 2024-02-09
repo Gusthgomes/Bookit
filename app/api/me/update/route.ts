38915964d870aaa0389796030b82eb8961d47abf
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-09 16:30:50 -0300

    feat: protected page and user

---
+++ app/api/me/update/route.ts
@@ -0,0 +1,21 @@
+import { dbConnect } from "@/backend/config/dbConnect";
+import { updateProfile } from "@/backend/controllers/authControllers";
+import { isAuthenticated } from "@/backend/middleware/auth";
+import { createEdgeRouter } from "next-connect";
+import { NextRequest } from "next/server";
+
+interface RequestContext {
+    params: {
+        id: string;
+    };
+}
+
+const router = createEdgeRouter<NextRequest, RequestContext>();
+
+dbConnect();
+
+router.use(isAuthenticated).put(updateProfile);
+
+export async function PUT(request: NextRequest, ctx: RequestContext) {
+    return router.run(request, ctx);
+};
