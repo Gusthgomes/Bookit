cf5075f17e297daaf83508995d854314fdf41f07
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-09 17:50:29 -0300

    fix: create type to the IUser

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,6 +10,10 @@ type Credential = {
     password: string;
 }
 
+type Token = {
+    user: IUser;
+}
+
 async function auth(req: NextApiRequest, res: NextApiResponse) {
     return await NextAuth(req, res, {
         session: {
@@ -42,7 +46,16 @@ async function auth(req: NextApiRequest, res: NextApiResponse) {
 
         callbacks: {
             jwt: async ({ token, user }) => {
+
+                const jwtToken = token as Token;
                 user && (token.user = user);
+
+                // Update session when user is updated
+                if(req.url?.includes('/api/auth/session?update')) {
+                    // Hit the Database and return the updated user
+                    const updatedUser = await User.findById(jwtToken?.user?._id);
+                    token.user = updatedUser;
+                }
 
                 return token;
             },
@@ -57,6 +70,9 @@ async function auth(req: NextApiRequest, res: NextApiResponse) {
             },
         },
 
+        pages: {
+            signIn: '/login'
+        },
         secret: process.env.NEXTAUTH_SECRET
     });
 };
