10c92163fa1a837ebfa663a190658041d07fa721
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-09 17:50:58 -0300

    feat: create updateProfile page

---
+++ app/me/update/page.tsx
@@ -0,0 +1,12 @@
+import UpdateProfile from '@/components/user/UpdateProfile';
+import React from 'react'
+
+const UpdateProfilePage = () => {
+    return (
+        <div>
+            <UpdateProfile/>
+        </div>
+    )
+}
+
+export default UpdateProfilePage;
