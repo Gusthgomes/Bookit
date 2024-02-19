95a67ada696e9beddbcebae6e37a4817d21f2752
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-19 16:00:30 -0300

    feat: create new page update password

---
+++ app/me/update_password/page.tsx
@@ -0,0 +1,17 @@
+import UpdatePassword from '@/components/user/UpdatePassword';
+import React from 'react'
+
+export const metadata = {
+    title: 'Update password',
+    description: 'Update password',
+}
+
+const UpdatePasswordPage = () => {
+    return (
+        <div>
+            <UpdatePassword/>
+        </div>
+    )
+}
+
+export default UpdatePasswordPage;
