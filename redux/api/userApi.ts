df0a3ae71d133364ce2375aadd94fd06b8c3812e
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-19 16:01:26 -0300

    feat: create route to update password

--- redux/api/userApi.ts
+++ redux/api/userApi.ts
@@ -22,7 +22,17 @@ export const userApi = createApi({
                 };
             },
         }),
+
+        updatePassword: builder.mutation({
+            query(body) {
+                return {
+                    url: '/me/update_password',
+                    method: 'PUT',
+                    body
+                };
+            },
+        }),
     }),
 });
 
-export const { useUpdateProfileMutation, useLazyUpdateSessionQuery } = userApi;
+export const { useUpdateProfileMutation, useLazyUpdateSessionQuery, useUpdatePasswordMutation } = userApi;
