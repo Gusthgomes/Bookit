7d0d724031e4e751bb75203d4047f80862420814
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-19 16:00:44 -0300

    fix: add metadata

--- app/me/update/page.tsx
+++ app/me/update/page.tsx
@@ -1,6 +1,11 @@
 import UpdateProfile from '@/components/user/UpdateProfile';
 import React from 'react'
 
+export const metadata = {
+    title: 'Update profile',
+    description: 'Update profile',
+}
+
 const UpdateProfilePage = () => {
     return (
         <div>
