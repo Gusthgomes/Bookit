0d9404b0d8e27e3c7541951cbe550e4deb38387e
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-20 15:50:33 -0300

    feat: add generate password reset token

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,5 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
 
 export interface IUser extends Document {
     name: string;
@@ -14,6 +15,7 @@ export interface IUser extends Document {
     resetPasswordToken: string;
     resetPasswordExpire: Date;
     comparePassword(enteredPassword: string): Promise<boolean>;
+    getResetPasswordToken(): string;
 }
 
 const userSchema: Schema<IUser> = new mongoose.Schema({
@@ -68,5 +70,19 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.comparePassword = async function(enteredPassword: string): Promise<boolean> {
     return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// Generate password reset token
+userSchema.methods.getResetPasswordToken = function(): string {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash the token
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    // Set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+    return resetToken;
+}
 
 export default mongoose.models.User || mongoose.model<IUser>('User', userSchema);
