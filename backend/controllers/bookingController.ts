5732708e618ef1c6ab26edc8e960376eef67d8b5
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-21 16:16:03 -0300

    feat: create new controller to register booking

---
+++ backend/controllers/bookingController.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { catchAsyncErrors } from "../middleware/catchAsyncErrors";
+import Booking from "../models/Booking";
+
+
+export const newBooking = catchAsyncErrors(async (req: NextRequest) => {
+    const body = await req.json();
+
+    const {
+        room,
+        checkInDate,
+        checkOutDate,
+        daysOfStay,
+        amountPaid,
+        paymentInfo,
+    } = body;
+
+    const booking = await Booking.create({
+        room,
+        user: req.user._id,
+        checkInDate,
+        checkOutDate,
+        daysOfStay,
+        amountPaid,
+        paymentInfo,
+        paidAt: Date.now(),
+    });
+
+    return NextResponse.json({
+        booking,
+    })
+    
+});
