e7eb088afde93c756d4886e4d76ae8d3bf7895fd
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-21 17:04:42 -0300

    feat: implement if check room booking availability

--- backend/controllers/bookingController.ts
+++ backend/controllers/bookingController.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { catchAsyncErrors } from "../middleware/catchAsyncErrors";
-import Booking from "../models/Booking";
+import Booking, { IBooking } from "../models/Booking";
 
 
+// Create new booking => /api/bookings
 export const newBooking = catchAsyncErrors(async (req: NextRequest) => {
     const body = await req.json();
 
@@ -28,6 +29,30 @@ export const newBooking = catchAsyncErrors(async (req: NextRequest) => {
 
     return NextResponse.json({
         booking,
+    })
+    
+});
+
+// check Room Booking Availability => /api/bookings/check
+export const checkRoomBookingAvailability = catchAsyncErrors(async (req: NextRequest) => {
+    const { searchParams } = new URL(req.url)
+    const roomId = searchParams.get('roomId');
+
+    const checkInDate = new Date(searchParams.get('checkInDate') as string);
+    const checkOutDate = new Date(searchParams.get('checkOutDate') as string);
+
+    const bookings: IBooking[] = await Booking.find({
+        room: roomId,
+        $and: [
+            { checkInDate: { $lte: checkOutDate } },
+            { checkOutDate: { $gte: checkInDate } }
+        ],
+    });
+
+    const isAvailable: boolean = bookings.length === 0;
+
+    return NextResponse.json({
+        isAvailable,
     })
     
 });
