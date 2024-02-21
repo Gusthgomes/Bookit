1ab3c2311abfd6a64b81d22bb6780dd2537abbb9
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-21 16:16:39 -0300

    feat: implement booking register function

--- components/room/BookingDatePicker.tsx
+++ components/room/BookingDatePicker.tsx
@@ -1,5 +1,7 @@
 'use client';
 import { IRoom } from '@/backend/models/Room'
+import { calculateDaysOfStay } from '@/helpers/helpers';
+import { useNewBookingMutation } from '@/redux/api/bookingApi';
 import React, { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
@@ -10,8 +12,27 @@ interface Props {
 
 const BookingDatePicker = ({ room }: Props) => {
 
-    const [ checkInDate, setCheckInDate ] = useState(new Date())
-    const [ checkOutDate, setCheckOutDate ] = useState(new Date())
+    const [ checkInDate, setCheckInDate ] = useState(new Date());
+    const [ checkOutDate, setCheckOutDate ] = useState(new Date());
+    const [ daysOfStay, setDaysOfStay ] = useState(0);
+
+    const [newBookin] = useNewBookingMutation();
+
+    const bookRoom = () => {
+        const bookingData = {
+            room: room?._id,
+            checkInDate,
+            checkOutDate,
+            daysOfStay,
+            amountPaid: room.pricePerNight * daysOfStay,
+            paymentInfo: {
+                id: 'STRIPE_ID',
+                status: 'PAID',
+            },
+        }
+
+        newBookin(bookingData);
+    }
 
     const onChange = (dates: Date[]) => {
         const [ checkInDate, checkOutDate ] = dates;
@@ -19,21 +40,23 @@ const BookingDatePicker = ({ room }: Props) => {
         setCheckOutDate(checkOutDate);
 
         if(checkInDate && checkOutDate) {
-            console.log('checkInDate:', checkInDate);
-            console.log('checkOutDate:', checkOutDate);
+            const days = calculateDaysOfStay(checkInDate, checkOutDate);
+
+            setDaysOfStay(days);
+
             // check booking availability
         }
     };
 
     return (
         <div className='booking-card shadow p-4'>
-            <p className='price-per-night'>
+            <p className='price-per-night text-center'>
                 <b>${room?.pricePerNight}</b> / night
             </p>
 
             <hr/>
 
-            <p className='mt5 mb-3'>
+            <p className='mt5 mb-3 text-center'>
                 Pick Check In & Check Out Date
             </p>
 
@@ -47,6 +70,10 @@ const BookingDatePicker = ({ room }: Props) => {
                 selectsRange
                 inline
             />
+
+            <button className='btn py-3 form-btn w-100' onClick={bookRoom}>
+                Pay
+            </button>
 
         </div>
     )
