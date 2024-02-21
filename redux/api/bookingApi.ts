2cda23b3677c6d9368e7c304a4e82f3214b832f2
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-21 17:05:46 -0300

    feat: create check booking availability query

--- redux/api/bookingApi.ts
+++ redux/api/bookingApi.ts
@@ -14,10 +14,15 @@ export const bookingApi = createApi({
             },
         }),
 
-        
-
-        
+        checkBookingAvailability: builder.query({
+            query({ id, checkInDate, checkOutDate }) {
+                return {
+                    url: `/bookings/check_room_availability?roomId=${id}&checkInDate=${checkInDate}&checkOutDate=${checkOutDate}`,
+                    
+                };
+            },
+        }),
     }),
 });
 
-export const { useNewBookingMutation} = bookingApi;
+export const { useNewBookingMutation, useLazyCheckBookingAvailabilityQuery } = bookingApi;
