7b8c51e8adf6e27395d163ce0801e1c60bd5e074
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-26 21:06:30 -0300

    feat: create stripe controller

---
+++ backend/controllers/paymentControllers.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import { catchAsyncErrors } from "../middleware/catchAsyncErrors";
+import Room from "../models/Room";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// Generate stripe checkout session => /api/payment/checkout_session/:roomId
+export const stripeCheckoutSession = catchAsyncErrors(
+  async (req: NextRequest, { params }: { params: { id: string } }) => {
+    const { searchParams } = new URL(req.url);
+
+    const checkInDate = searchParams.get("checkInDate");
+    const checkOutDate = searchParams.get("checkOutDate");
+    const daysOfStay = searchParams.get("daysOfStay");
+    const roomAmount = searchParams.get("amount");
+
+    // Get room details
+    const room = await Room.findById(params.id);
+
+    // Create stripe checkout session
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      success_url: `${process.env.API_URL}/bookings/me`,
+      cancel_url: `${process.env.API_URL}/room/${room?._id}`,
+      customer_email: req.user.email,
+      client_reference_id: params?.id,
+      metadata: { checkInDate, checkOutDate, daysOfStay },
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            unit_amount: Number(roomAmount) * 100,
+            product_data: {
+              name: room?.name,
+              description: room?.description,
+              images: [`${room?.images[0]?.url}`],
+            },
+          },
+          quantity: 1,
+        },
+      ],
+    });
+
+    return NextResponse.json(session);
+  }
+);
