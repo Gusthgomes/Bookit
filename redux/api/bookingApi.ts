ce454b21a0130b13590e6ceceb2247ad1d6368fd
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-26 21:05:36 -0300

    feat: create stripe checkout api

--- redux/api/bookingApi.ts
+++ redux/api/bookingApi.ts
@@ -1,41 +1,54 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const bookingApi = createApi({
-    reducerPath: "bookingApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
-    endpoints: (builder) => ({
-        newBooking: builder.mutation({
-            query(body) {
-                return {
-                    url: '/bookings',
-                    method: 'POST',
-                    body
-                };
-            },
-        }),
-
-        checkBookingAvailability: builder.query({
-            query({ id, checkInDate, checkOutDate }) {
-                return {
-                    url: `/bookings/check_room_availability?roomId=${id}&checkInDate=${checkInDate}&checkOutDate=${checkOutDate}`,
-                    
-                };
-            },
-        }),
-
-        getBookedDates: builder.query({
-            query({ id }) {
-                return {
-                    url: `/bookings/booked_dates?roomId=${id}`,
-                    
-                };
-            },
-        }),
+  reducerPath: "bookingApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  endpoints: (builder) => ({
+    newBooking: builder.mutation({
+      query(body) {
+        return {
+          url: "/bookings",
+          method: "POST",
+          body,
+        };
+      },
     }),
+
+    checkBookingAvailability: builder.query({
+      query({ id, checkInDate, checkOutDate }) {
+        return {
+          url: `/bookings/check_room_availability?roomId=${id}&checkInDate=${checkInDate}&checkOutDate=${checkOutDate}`,
+        };
+      },
+    }),
+
+    getBookedDates: builder.query({
+      query({ id }) {
+        return {
+          url: `/bookings/booked_dates?roomId=${id}`,
+        };
+      },
+    }),
+
+    stripeCheckout: builder.query({
+      query({ id, checkoutData }) {
+        return {
+          url: `/payment/checkout_session/${id}`,
+          params: {
+            checkInDate: checkoutData.checkInDate,
+            checkOutDate: checkoutData.checkOutDate,
+            daysOfStay: checkoutData.daysOfStay,
+            amount: checkoutData.amount,
+          },
+        };
+      },
+    }),
+  }),
 });
 
-export const { useNewBookingMutation, 
-    useLazyCheckBookingAvailabilityQuery,
-    useGetBookedDatesQuery,
-    } = bookingApi;
-
+export const {
+  useNewBookingMutation,
+  useLazyCheckBookingAvailabilityQuery,
+  useGetBookedDatesQuery,
+  useLazyStripeCheckoutQuery,
+} = bookingApi;
