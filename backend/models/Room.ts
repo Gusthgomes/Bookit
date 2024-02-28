2596f1b1fc494ae028c027ccd5cd112c42979dd0
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-27 21:30:16 -0300

    chore: change properties of IRoom interface

--- backend/models/Room.ts
+++ backend/models/Room.ts
@@ -1,198 +1,192 @@
 import mongoose, { Schema, Document } from "mongoose";
+import { IUser } from "./User";
 
 export interface IImage {
-    public_id: string,
-    url: string,
-
+  public_id: string;
+  url: string;
 }
 
 export interface IReview {
-    user: mongoose.Schema.Types.ObjectId,
-    name: string,
-    rating: number,
-    comment: string,
-
+  user: IUser;
+  name: string;
+  rating: number;
+  comment: string;
 }
 
 export interface ILocation {
-    type: string,
-    coordinates: number[],
-    formattedAddress: string,
-    city: string,
-    state: string,
-    zipCode: string,
-    country: string,
-
+  type: string;
+  coordinates: number[];
+  formattedAddress: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
 }
 
 export interface IRoom extends Document {
-    name: string,
-    description: string,
-    pricePerNight: number,
-    address: string,
-    location: ILocation,
-    guestCapacity: number,
-    numOfBeds: number,
-    isInternet: boolean,
-    isBreakfast: boolean,
-    isAirConditioned: boolean,
-    isPetsAllowed: boolean,
-    isRoomCleaning: boolean,
-    ratings: number,
-    numOfReviews: number,
-    images: IImage[],
-    category: string,
-    reviews: IReview[],
-    user: mongoose.Schema.Types.ObjectId,
-    createdAt: Date,
+  name: string;
+  description: string;
+  pricePerNight: number;
+  address: string;
+  location: ILocation;
+  guestCapacity: number;
+  numOfBeds: number;
+  isInternet: boolean;
+  isBreakfast: boolean;
+  isAirConditioned: boolean;
+  isPetsAllowed: boolean;
+  isRoomCleaning: boolean;
+  ratings: number;
+  numOfReviews: number;
+  images: IImage[];
+  category: string;
+  reviews: IReview[];
+  user: IUser;
+  createdAt: Date;
 }
 
 const roomSchema: Schema<IRoom> = new Schema({
-    name: {
+  name: {
+    type: String,
+    required: [true, "Please enter room name"],
+    trim: true,
+    maxLength: [200, "room name cannot exceed 200 characters"],
+  },
+
+  description: {
+    type: String,
+    required: [true, "Please enter room description"],
+    trim: true,
+    maxLength: [1000, "room name cannot exceed 1000 characters"],
+  },
+
+  pricePerNight: {
+    type: Number,
+    required: [true, "Please enter room price per night"],
+    defaultValue: 0.0,
+  },
+
+  address: {
+    type: String,
+    required: [true, "please enter room address"],
+  },
+
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+    },
+
+    coordinates: {
+      type: [Number],
+      index: "2dsphere",
+    },
+
+    formattedAddress: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String,
+  },
+
+  guestCapacity: {
+    type: Number,
+    required: [true, "Please enter room guest capacity"],
+  },
+
+  numOfBeds: {
+    type: Number,
+    required: [true, "Please enter number of beds in room"],
+  },
+
+  isInternet: {
+    type: Boolean,
+    default: false,
+  },
+
+  isBreakfast: {
+    type: Boolean,
+    default: false,
+  },
+
+  isAirConditioned: {
+    type: Boolean,
+    default: false,
+  },
+
+  isPetsAllowed: {
+    type: Boolean,
+    default: false,
+  },
+
+  isRoomCleaning: {
+    type: Boolean,
+    default: false,
+  },
+
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+
+  images: [
+    {
+      public_id: {
         type: String,
-        required: [true, 'Please enter room name'],
-        trim: true,
-        maxLength: [200, 'room name cannot exceed 200 characters']
-    },
-
-    description: {
+        required: true,
+      },
+      url: {
         type: String,
-        required: [true, 'Please enter room description'],
-        trim: true,
-        maxLength: [1000, 'room name cannot exceed 1000 characters']
+        required: true,
+      },
     },
+  ],
 
-    pricePerNight: {
-        type: Number,
-        required: [true, 'Please enter room price per night'],
-        defaultValue: 0.0
+  category: {
+    type: String,
+    required: [true, "Please enter room category"],
+    enum: {
+      values: ["King", "Single", "Twins"],
+      message: "Please select correct category for room",
     },
+  },
 
-    address: {
-        type: String,
-        required: [true, 'please enter room address']
-    },
-
-    location: {
-        type: {
-            type: String,
-            enum: ['Point']
-        },
-
-        coordinates: {
-            type: [Number],
-            index: '2dsphere'
-        },
-
-        formattedAddress: String,
-        city: String,
-        state: String,
-        zipCode: String,
-        country: String,
-    },
-
-    guestCapacity: {
-        type: Number,
-        required: [true, 'Please enter room guest capacity'],
-    },
-
-    numOfBeds: {
-        type: Number,
-        required: [true, 'Please enter number of beds in room'],
-    },
-
-    isInternet: {
-        type: Boolean,
-        default: false
-    },
-
-    isBreakfast: {
-        type: Boolean,
-        default: false
-    },
-
-    isAirConditioned: {
-        type: Boolean,
-        default: false
-    },
-
-    isPetsAllowed: {
-        type: Boolean,
-        default: false
-    },
-
-    isRoomCleaning: {
-        type: Boolean,
-        default: false
-    },
-    
-    ratings: {
-        type: Number,
-        default: 0
-    },
-
-    numOfReviews: {
-        type: Number,
-        default: 0
-    },
-
-    images: [
-        {
-            public_id: {
-                type: String,
-                required: true
-            },
-            url: {
-                type: String,
-                required: true
-            },
-        },
-    ],
-
-    category: {
-        type: String,
-        required: [true, 'Please enter room category'],
-        enum: {
-            values: [
-                'King',
-                'Single',
-                'Twins'
-            ],
-            message: 'Please select correct category for room'
-        },
-    },
-
-    reviews: [
-        {
-            user: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User',
-                required: true
-            },
-
-            rating: {
-                type: Number,
-                required: true
-            },
-
-            comment: {
-                type: String,
-                required: true
-            },
-        },
-    ],
-
-    user: {
+  reviews: [
+    {
+      user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: false,
-    },
+        ref: "User",
+        required: true,
+      },
 
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
+      rating: {
+        type: Number,
+        required: true,
+      },
 
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export default mongoose.models.Room || mongoose.model<IRoom>('Room', roomSchema);
+export default mongoose.models.Room ||
+  mongoose.model<IRoom>("Room", roomSchema);
