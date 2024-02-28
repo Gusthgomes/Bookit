718f128a9afc8e808d2d87501042c53ef39110a2
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-27 21:31:03 -0300

    chore: add interface and reviews information

--- components/review/ListReview.tsx
+++ components/review/ListReview.tsx
@@ -1,45 +1,51 @@
-import React from 'react'
+import { IReview } from "@/backend/models/Room";
+import React from "react";
 
-const ListReview = () => {
-    return (
-        <div className="reviews w-75 mb-5">
-            <h3>3 Reviews</h3>
-            <hr />
-        
-            <div className="review-card my-3">
-        
-            <div className="row">
-                <div className="col-3 col-lg-1">
-                <img
-                    src="./images/avatar.jpg"
-                    alt="John Doe"
-                    width="60"
-                    height="60"
-                    className="rounded-circle"
-                />
-                </div>
-                <div className="col-9 col-lg-11">
-                <div className="star-ratings">
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star-half"></i>
-                </div>
-                <p className="review_user mt-1">by John Doe</p>
-            
-                <p className="review_comment">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                    consectetur, mi nec tristique vehicula, elit tellus vulputate
-                    ex, nec bibendum libero elit at orci.
-                </p>
-                </div>
-                <hr />
-            </div>
-            </div>
-        
-        </div>
-    )
+interface Props {
+  reviews: IReview[];
 }
 
-export default ListReview
+const ListReview = ({ reviews }: Props) => {
+  return (
+    <div className="reviews w-75 mb-5">
+      <h3>{reviews.length} Reviews</h3>
+      <hr />
+
+      {reviews?.map((review) => (
+        <div className="review-card my-3">
+          <div className="row">
+            <div className="col-3 col-lg-1">
+              <img
+                src={
+                  review?.user?.avatar
+                    ? review?.user?.avatar?.url
+                    : "/images/default_avatar.jpg"
+                }
+                alt={review?.user?.name}
+                width={60}
+                height={60}
+                className="rounded-circle"
+              />
+            </div>
+            <div className="col-9 col-lg-11">
+              <div className="rating-outer">
+                <div
+                  className="rating-inner"
+                  style={{ width: `${(review?.rating / 5) * 100}%` }}
+                >
+                  {review?.rating} stars
+                </div>
+              </div>
+              <p className="review_user mt-1">by {review?.user?.name}</p>
+
+              <p className="review_comment">{review?.comment}</p>
+            </div>
+            <hr />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ListReview;
