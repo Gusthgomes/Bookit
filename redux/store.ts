21d6d0d1bc83768e64cdae4bb25b709d72dcaa33
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-27 21:27:11 -0300

    feat: add roomApi

--- redux/store.ts
+++ redux/store.ts
@@ -1,23 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './features/userSlice';
+import userReducer from "./features/userSlice";
 import { authApi } from "./api/authApi";
 import { userApi } from "./api/userApi";
 import { bookingApi } from "./api/bookingApi";
+import { roomApi } from "./api/roomApi";
 
 export const store = configureStore({
-    reducer: {
-        auth: userReducer,
-        [authApi.reducerPath]: authApi.reducer,
-        [userApi.reducerPath]: userApi.reducer,
-        [bookingApi.reducerPath]: bookingApi.reducer,
-        
-    },
-    middleware: (getDefaultMiddleware) => 
-        getDefaultMiddleware().concat([
-            authApi.middleware, 
-            userApi.middleware,
-            bookingApi.middleware,
-        ]),
+  reducer: {
+    auth: userReducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [bookingApi.reducerPath]: bookingApi.reducer,
+    [roomApi.reducerPath]: roomApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([
+      authApi.middleware,
+      userApi.middleware,
+      bookingApi.middleware,
+      roomApi.middleware,
+    ]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
