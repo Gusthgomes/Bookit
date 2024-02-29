dd28b64f5814621b2d6fd0f394b98df0953b0cfa
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-28 21:40:22 -0300

    feat: create middleware to authorize roles

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,24 @@
 import { withAuth } from "next-auth/middleware";
+import { IUser } from "./backend/models/User";
+import { NextResponse } from "next/server";
 
 export default withAuth(
-    function middleware(req) {
+  function middleware(req) {
+    // authorize roles
+    const url = req?.nextUrl?.pathname;
+    const user = req?.nextauth?.token?.user as IUser;
 
+    if (url?.startsWith("/admin") && user?.role !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url));
     }
-)
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
-    matcher: [
-        '/me/:path*',
-        '/bookings/:path*',
-    ]
+  matcher: ["/me/:path*", "/bookings/:path*", "/admin/:path*"],
 };
