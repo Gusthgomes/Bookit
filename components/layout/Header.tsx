4c1cbbe5c6d560968cc2e66d0ce41f2bac81780f
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-02-28 21:44:40 -0300

    feat: remove url if user isnt admin

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import React, { useEffect } from "react";
@@ -7,23 +7,22 @@ import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setIsAuthenticated, setUser } from "@/redux/features/userSlice";
 
 const Header = () => {
-
   const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.auth);
 
   const { data } = useSession();
 
-  useEffect( () => {
-    if(data) {
+  useEffect(() => {
+    if (data) {
       dispatch(setUser(data.user));
       dispatch(setIsAuthenticated(true));
     }
   }, [data]);
-  
+
   const logoutHandler = () => {
     signOut();
-  }
-  
+  };
+
   return (
     <nav className="navbar sticky-top py-2">
       <div className="container">
@@ -40,7 +39,7 @@ const Header = () => {
         </div>
 
         <div className="col-6 col-lg-3 mt-3 mt-md-0 text-end">
-          { user ? (
+          {user ? (
             <div className="ml-4 dropdown d-line">
               <button
                 className="btn dropdown-toggle"
@@ -51,49 +50,62 @@ const Header = () => {
               >
                 <figure className="avatar avatar-nav">
                   <img
-                    src={ user?.avatar ? user?.avatar?.url : "/images/default_avatar.jpg" }
+                    src={
+                      user?.avatar
+                        ? user?.avatar?.url
+                        : "/images/default_avatar.jpg"
+                    }
                     alt="Avatar"
                     className="rounded-circle placeholder-glow"
                     height="50"
                     width="50"
                   />
                 </figure>
-                <span className="placeholder-glow ps-1"> { user?.name }</span>
+                <span className="placeholder-glow ps-1"> {user?.name}</span>
               </button>
 
               <div
                 className="dropdown-menu w-100"
                 aria-labelledby="dropdownMenuButton1"
               >
-                <Link href="/admin/dashboard" className="dropdown-item">
-                  Dashboard
-                </Link>
+                {user?.role === "admin" && (
+                  <Link href="/admin/dashboard" className="dropdown-item">
+                    Dashboard
+                  </Link>
+                )}
                 <Link href="/bookings/me" className="dropdown-item">
                   My Bookings
                 </Link>
                 <Link href="/me/update" className="dropdown-item">
                   Profile
                 </Link>
-                <Link href="/" className="dropdown-item text-danger" onClick={ logoutHandler }>
+                <Link
+                  href="/"
+                  className="dropdown-item text-danger"
+                  onClick={logoutHandler}
+                >
                   Logout
                 </Link>
               </div>
             </div>
           ) : (
             <>
-              { data === undefined && (
+              {data === undefined && (
                 <div className="placeholder-glow">
                   <figure className=" avatar avatar-nv placeholder bg-secondary"></figure>
                   <span className="placeholder w-25 bg-secondary ms-2"></span>
                 </div>
               )}
-              { data === null && (
-                <Link href='/login' className="btn btn-danger px-4 text-white login-header-btn float-right">
+              {data === null && (
+                <Link
+                  href="/login"
+                  className="btn btn-danger px-4 text-white login-header-btn float-right"
+                >
                   Login
                 </Link>
               )}
             </>
-            )}
+          )}
         </div>
       </div>
     </nav>
