512f301f73ab233cb69317b43149616b1549cca9
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-04 22:02:42 -0300

    fix: change elements of DatePicker

--- components/admin/Dashboard.tsx
+++ components/admin/Dashboard.tsx
@@ -2,6 +2,10 @@
 
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import SalesStats from "./SalesStats";
+import { SalesChart } from "../charts/SalesCharts";
+import { TopPerformingChart } from "../charts/TopPerformingChart";
 
 const Dashboard = () => {
   const [startDate, setStartDate] = useState(new Date());
@@ -22,10 +26,10 @@ const Dashboard = () => {
           />
         </div>
 
-        <div className="mb-3 me-4">
+        <div className="mb-3">
           <label className="form-label d-block">End date</label>
           <DatePicker
-            selected={startDate}
+            selected={endDate}
             onChange={(date: any) => setEndDate(date)}
             selectsEnd
             startDate={startDate}
@@ -36,6 +40,19 @@ const Dashboard = () => {
         </div>
 
         <button className="btn form-btn ms-4 mt-3 px-5">Fetch</button>
+      </div>
+      <SalesStats />
+
+      <div className="row">
+        <div className="col-12 col-lg-8">
+          <h4 className="my-5 text-center">Sales History</h4>
+          <SalesChart />
+        </div>
+
+        <div className="col-12 col-lg-4 text-center">
+          <h4 className="my-5">Top Performing Rooms</h4>
+          <TopPerformingChart />
+        </div>
       </div>
     </div>
   );
