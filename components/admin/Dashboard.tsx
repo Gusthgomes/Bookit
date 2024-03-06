f39570baee89304aec6e2cd6ac9eaa433cd240d4
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-05 22:16:56 -0300

    chore: add props in SalesChart component

--- components/admin/Dashboard.tsx
+++ components/admin/Dashboard.tsx
@@ -73,7 +73,7 @@ const Dashboard = () => {
       <div className="row">
         <div className="col-12 col-lg-8">
           <h4 className="my-5 text-center">Sales History</h4>
-          <SalesChart />
+          <SalesChart salesData={data?.sixMonthSalesData} />
         </div>
 
         <div className="col-12 col-lg-4 text-center">
