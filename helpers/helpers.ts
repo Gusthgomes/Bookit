0efa3984f6d359e61100617dc0632347ff263321
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-05 21:32:10 -0300

    chore: create add commas to amount

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -1,8 +1,18 @@
-import moment from 'moment';
+import moment from "moment";
 
 export const calculateDaysOfStay = (checkInDate: Date, checkOutDate: Date) => {
-    const startDate = moment(checkInDate);
-    const endDate = moment(checkOutDate);
+  const startDate = moment(checkInDate);
+  const endDate = moment(checkOutDate);
 
-    return endDate.diff(startDate, 'days') + 1;
-}
+  return endDate.diff(startDate, "days") + 1;
+};
+
+export const addCommasToAmount = (amount: string) => {
+  amount = amount.toString();
+
+  const parts = amount.split(".");
+
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return parts.join(".");
+};
