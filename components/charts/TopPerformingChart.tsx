2c4513392414ad002fd176999e5b2227efaa2382
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-06 21:24:55 -0300

    feat: receiving props and work with then

--- components/charts/TopPerformingChart.tsx
+++ components/charts/TopPerformingChart.tsx
@@ -4,27 +4,36 @@ import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ["Red", "Blue", "Yellow"],
-  datasets: [
-    {
-      label: "# of bookings",
-      data: [12, 19, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.5)",
-        "rgba(54, 162, 235, 0.5)",
-        "rgba(255, 206, 86, 0.5)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+interface Room {
+  roomName: string;
+  bookingCount: number;
+}
 
-export function TopPerformingChart() {
+interface Props {
+  rooms: Room[];
+}
+
+export function TopPerformingChart({ rooms }: Props) {
+  const data = {
+    labels: rooms?.map((room) => room?.roomName),
+    datasets: [
+      {
+        label: "# of bookings",
+        data: rooms?.map((room) => room?.bookingCount),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return <Doughnut data={data} />;
 }
