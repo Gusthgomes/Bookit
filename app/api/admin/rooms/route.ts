46c58b87a5bf0cdfd3deeecd0ca7e5ee7c06cfcc
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-07 22:12:05 -0300

    feat: create new admin route to get all rooms

--- app/api/admin/rooms/route.ts
+++ app/api/admin/rooms/route.ts
@@ -1,18 +1,22 @@
 import { dbConnect } from "@/backend/config/dbConnect";
-import { newRoom } from "@/backend/controllers/roomControllers";
+import { allAdminRooms, newRoom } from "@/backend/controllers/roomControllers";
+import { authorizeRoles, isAuthenticated } from "@/backend/middleware/auth";
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
 
-interface RequestContext {
-    
-}
+interface RequestContext {}
 
-const route = createEdgeRouter<NextRequest, RequestContext>()
+const route = createEdgeRouter<NextRequest, RequestContext>();
 
 dbConnect();
 
-route.post(newRoom);
+route.use(isAuthenticated, authorizeRoles("admin")).post(newRoom);
+route.use(isAuthenticated, authorizeRoles("admin")).get(allAdminRooms);
 
 export async function POST(request: NextRequest, ctx: RequestContext) {
-    return route.run(request, ctx);
+  return route.run(request, ctx);
+}
+
+export async function GET(request: NextRequest, ctx: RequestContext) {
+  return route.run(request, ctx);
 }
