e701ddd22b24f3f473190a91dc90cee9ef645d0d
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-11 21:51:25 -0300

    feat: add new body request in const newUser

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -40,6 +40,8 @@ export const allRooms = catchAsyncErrors(async (req: NextRequest) => {
 export const newRoom = catchAsyncErrors(async (req: NextRequest) => {
   const body = await req.json();
 
+  body.user = req.user._id;
+
   const room = await Room.create(body);
 
   return NextResponse.json({
