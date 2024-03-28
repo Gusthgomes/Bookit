b26c9bf700fb7aa093df0e3a446d9dfa3ea7088d
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-27 21:40:44 -0300

    feat: create route to revalidate args

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from "next/server";
+import { revalidateTag } from "next/cache";
+
+export async function POST(request: NextRequest) {
+  const secret = request.nextUrl.searchParams.get("secret");
+  const tag = request.nextUrl.searchParams.get("tag");
+
+  if (secret !== process.env.REVALIDATE_TOKEN) {
+    return NextResponse.json(
+      {
+        errMessage: "Invalid Secret",
+      },
+      { status: 401 }
+    );
+  }
+
+  if (!tag) {
+    return NextResponse.json(
+      {
+        errMessage: "Missing tag param",
+      },
+      { status: 400 }
+    );
+  }
+
+  revalidateTag(tag);
+
+  return NextResponse.json({
+    revalided: true,
+    now: Date.now(),
+  });
+}
