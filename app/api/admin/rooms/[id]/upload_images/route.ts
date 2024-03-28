41c5129201a5050d93e8011d614979430a64fc51
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-27 21:39:31 -0300

    feat: create route to upload room image

---
+++ app/api/admin/rooms/[id]/upload_images/route.ts
@@ -0,0 +1,17 @@
+import { dbConnect } from "@/backend/config/dbConnect";
+import { uploadRoomImages } from "@/backend/controllers/roomControllers";
+import { authorizeRoles, isAuthenticated } from "@/backend/middleware/auth";
+import { createEdgeRouter } from "next-connect";
+import { NextRequest } from "next/server";
+
+interface RequestContext {}
+
+const route = createEdgeRouter<NextRequest, RequestContext>();
+
+dbConnect();
+
+route.use(isAuthenticated, authorizeRoles("admin")).put(uploadRoomImages);
+
+export async function PUT(request: NextRequest, ctx: RequestContext) {
+  return route.run(request, ctx);
+}
