a6b7d6372fa952fb32f526c7c5ee3edd7ebbb9fe
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-28 21:07:09 -0300

    feat: create new controller to delete room images

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import Room, { IReview, IRoom } from "../models/Room";
+import Room, { IImage, IReview, IRoom } from "../models/Room";
 import ErrorHandler from "../utils/errorHandler";
 import { catchAsyncErrors } from "../middleware/catchAsyncErrors";
 import APIFilters from "../utils/apiFilters";
 import Booking from "../models/Booking";
-import { upload_file } from "../utils/cloudinary";
+import { delete_file, upload_file } from "../utils/cloudinary";
 
 // Get all rooms => /api/rooms
 export const allRooms = catchAsyncErrors(async (req: NextRequest) => {
@@ -97,7 +97,10 @@ export const deleteRoom = catchAsyncErrors(
       throw new ErrorHandler("Room not found", 404);
     }
 
-    // TODO - Delete images associated with the room
+    // Delete images associated with the room
+    for (let i = 0; i < room?.images?.length; i++) {
+      await delete_file(room?.images[i].public_id);
+    }
 
     await room.deleteOne();
 
@@ -187,9 +190,36 @@ export const uploadRoomImages = catchAsyncErrors(
     const uploader = async (images: string) =>
       upload_file(images, "bookit/rooms");
 
-    const urls = (await Promise.all(body?.images)).map(uploader);
+    const urls = await Promise.all((body?.images).map(uploader));
 
     room?.images?.push(...urls);
+
+    await room.save();
+
+    return NextResponse.json({
+      success: true,
+      room,
+    });
+  }
+);
+
+// Delete room images => api/admin/rooms/:id/delete_image
+export const deleteRoomImages = catchAsyncErrors(
+  async (req: NextRequest, { params }: { params: { id: string } }) => {
+    const room = await Room.findById(params.id);
+    const body = await req.json();
+
+    if (!room) {
+      throw new ErrorHandler("Room not found", 404);
+    }
+
+    const isDeleted = await delete_file(body?.imgId);
+
+    if (!isDeleted) {
+      room.images = room?.images.filter(
+        (img: IImage) => img.public_id !== body?.imgId
+      );
+    }
 
     await room.save();
 
