e1180004e6e9634d3dbc7f72c09973ef00a85012
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-03-28 21:08:31 -0300

    feat: add useRef and deleting rooms

--- components/admin/UploadRoomImages.tsx
+++ components/admin/UploadRoomImages.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { IImage, IRoom } from "@/backend/models/Room";
 import { revalidateTag } from "@/helpers/revalidate";
-import { useUploadRoomImagesMutation } from "@/redux/api/roomApi";
+import {
+  useDeleteRoomImagesMutation,
+  useUploadRoomImagesMutation,
+} from "@/redux/api/roomApi";
 import { useRouter } from "next/navigation";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import toast from "react-hot-toast";
+import ButtonLoader from "../layout/ButtonLoader";
 
 interface Props {
   data: {
@@ -13,14 +17,31 @@ interface Props {
 }
 
 const UploadRoomImages = ({ data }: Props) => {
+  const fileRef = useRef<HTMLInputElement>(null);
+
   const [images, setImages] = useState<string[]>([]);
   const [imagesPreview, setImagesPreview] = useState<string[]>([]);
   const [uploadingImages, setUploadingImages] = useState<IImage[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setUploadingImages(data?.room?.images);
+    }
+  }, [data]);
 
   const router = useRouter();
 
   const [uploadRoomImages, { error, isLoading, isSuccess }] =
     useUploadRoomImagesMutation();
+
+  const [
+    deleteRoomImages,
+    {
+      error: deleteError,
+      isLoading: isDeleteLoading,
+      isSuccess: isDeleteSuccess,
+    },
+  ] = useDeleteRoomImagesMutation();
 
   useEffect(() => {
     if (error && "data" in error) {
@@ -34,6 +55,19 @@ const UploadRoomImages = ({ data }: Props) => {
       toast.success("Images uploaded successfully!");
     }
   }, [error, isSuccess]);
+
+  useEffect(() => {
+    if (deleteError && "data" in deleteError) {
+      toast.error(deleteError?.data?.errMessage);
+    }
+
+    if (isDeleteSuccess) {
+      revalidateTag("RoomDetails");
+      setImagesPreview([]);
+      router.refresh();
+      toast.success("Images deleted successfully!");
+    }
+  }, [deleteError, isDeleteSuccess]);
 
   const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     const files = Array.from(e.target.files || []);
@@ -57,6 +91,23 @@ const UploadRoomImages = ({ data }: Props) => {
     uploadRoomImages({ id: data?.room?._id, body: { images } });
   };
 
+  const removeImagePreview = (imgUrl: string) => {
+    const filteredImagesPreview = imagesPreview.filter((img) => img != imgUrl);
+
+    setImagesPreview(filteredImagesPreview);
+    setImages(filteredImagesPreview);
+  };
+
+  const handleResetFileInput = () => {
+    if (fileRef.current) {
+      fileRef.current.value = "";
+    }
+  };
+
+  const handleImageDelete = (imgId: string) => {
+    deleteRoomImages({ id: data?.room?._id, body: { imgId } });
+  };
+
   return (
     <div className="row wrapper">
       <div className="col-10 col-lg-7 mt-5 mt-lg-0">
@@ -65,7 +116,7 @@ const UploadRoomImages = ({ data }: Props) => {
           action="/submit-product-images"
           method="POST"
         >
-          <h2 className="mb-4">Upload Product Images</h2>
+          <h2 className="mb-4">Upload Room Images</h2>
 
           <div className="form-group">
             <label htmlFor="customFile" className="form-label">
@@ -74,74 +125,89 @@ const UploadRoomImages = ({ data }: Props) => {
 
             <div className="custom-file">
               <input
+                ref={fileRef}
                 type="file"
                 name="product_images"
                 className="form-control"
                 id="customFile"
+                onChange={onChange}
+                onClick={handleResetFileInput}
                 multiple
                 required
               />
             </div>
 
-            <div className="new-images mt-4">
-              <p className="text-warning">New Images:</p>
-              <div className="row mt-4">
-                <div className="col-md-3 mt-2">
-                  <div className="card">
-                    <img
-                      src="path_to_image.jpg"
-                      alt="Card"
-                      className="card-img-top p-2"
-                      style={{ width: "100%", height: "80px" }}
-                    />
-                    <button
-                      style={{
-                        backgroundColor: "#dc3545",
-                        borderColor: "#dc3545",
-                      }}
-                      type="button"
-                      className="btn btn-block btn-danger cross-button mt-1 py-0"
-                    >
-                      <i className="fa fa-times"></i>
-                    </button>
-                  </div>
+            {imagesPreview?.length > 0 && (
+              <div className="new-images mt-4">
+                <p className="text-warning">New Images:</p>
+                <div className="row mt-4">
+                  {imagesPreview?.map((img) => (
+                    <div className="col-md-3 mt-2">
+                      <div className="card">
+                        <img
+                          src={img}
+                          alt="Image preview"
+                          className="card-img-top p-2"
+                          style={{ width: "100%", height: "80px" }}
+                        />
+                        <button
+                          style={{
+                            backgroundColor: "#dc3545",
+                            borderColor: "#dc3545",
+                          }}
+                          type="button"
+                          className="btn btn-block btn-danger cross-button mt-1 py-0"
+                          onClick={() => removeImagePreview(img)}
+                        >
+                          <i className="fa fa-times"></i>
+                        </button>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            </div>
+            )}
 
-            <div className="uploaded-images mt-4">
-              <p className="text-success">Product Uploaded Images:</p>
-              <div className="row mt-1">
-                <div className="col-md-3 mt-2">
-                  <div className="card">
-                    <img
-                      src="path_to_uploaded_image.jpg"
-                      alt="Card"
-                      className="card-img-top p-2"
-                      style={{ width: "100%", height: "80px" }}
-                    />
-                    <button
-                      style={{
-                        backgroundColor: "#dc3545",
-                        borderColor: "#dc3545",
-                      }}
-                      className="btn btn-block btn-danger cross-button mt-1 py-0"
-                      disabled
-                    >
-                      <i className="fa fa-trash"></i>
-                    </button>
-                  </div>
+            {uploadingImages?.length > 0 && (
+              <div className="uploaded-images mt-4">
+                <p className="text-success">Product Room Images:</p>
+                <div className="row mt-1">
+                  {uploadingImages?.map((img) => (
+                    <div className="col-md-3 mt-2">
+                      <div className="card">
+                        <img
+                          src={img?.url}
+                          alt={img?.url}
+                          className="card-img-top p-2"
+                          style={{ width: "100%", height: "80px" }}
+                        />
+                        <button
+                          style={{
+                            backgroundColor: "#dc3545",
+                            borderColor: "#dc3545",
+                          }}
+                          className="btn btn-block btn-danger cross-button mt-1 py-0"
+                          onClick={() => handleImageDelete(img.public_id)}
+                          disabled={isLoading || isDeleteLoading}
+                        >
+                          <i className="fa fa-trash"></i>
+                        </button>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            </div>
+            )}
           </div>
 
           <button
             id="register_button"
             type="submit"
             className="btn form-btn w-100 py-2"
+            onClick={submitHandler}
+            disabled={isLoading || isDeleteLoading}
           >
-            Upload
+            {isLoading ? <ButtonLoader /> : "UPLOAD"}
           </button>
         </form>
       </div>
