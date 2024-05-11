dc80429fcb379b6af8b1bd2dfe67df162d0fe787
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-05-11 10:29:57 -0300

    refactor: change import module

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,88 +1,93 @@
-import mongoose, { Document, Schema } from 'mongoose';
-import bcrypt from 'bcryptjs';
-import crypto from 'crypto';
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import * as crypto from "crypto";
 
 export interface IUser extends Document {
-    name: string;
-    email: string;
-    password: string;
-    avatar: {
-        public_id: string;
-        url: string;
-    };
-    role: string;
-    createdAt: Date;
-    resetPasswordToken: string;
-    resetPasswordExpire: Date;
-    comparePassword(enteredPassword: string): Promise<boolean>;
-    getResetPasswordToken(): string;
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  createdAt: Date;
+  resetPasswordToken: string;
+  resetPasswordExpire: Date;
+  comparePassword(enteredPassword: string): Promise<boolean>;
+  getResetPasswordToken(): string;
 }
 
 const userSchema: Schema<IUser> = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please enter your name'],
-    },
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
 
-    email: {
-        type: String,
-        required: [true, 'Please enter your email'],
-        unique: true,
-    },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+  },
 
-    password: {
-        type: String,
-        required: [true, 'Please enter your password'],
-        minlength: [6, 'Your password must be longer than 6 characters'],
-        select: false,
-    },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: [6, "Your password must be longer than 6 characters"],
+    select: false,
+  },
 
-    avatar: {
-        public_id: String,
-        url: String,
-    },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
 
-    role: {
-        type: String,
-        default: 'user',
-    },
+  role: {
+    type: String,
+    default: "user",
+  },
 
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
-
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
 // Encrypting password before saving user
-userSchema.pre('save', async function (next) {
-    if(!this.isModified('password')) {
-        next();
-    }
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-    this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 // Compare user password
-userSchema.methods.comparePassword = async function(enteredPassword: string): Promise<boolean> {
-    return await bcrypt.compare(enteredPassword, this.password);
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 // Generate password reset token
-userSchema.methods.getResetPasswordToken = function(): string {
-    // Generate token
-    const resetToken = crypto.randomBytes(20).toString('hex');
+userSchema.methods.getResetPasswordToken = function (): string {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-    // Hash the token
-    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  // Hash the token
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
-    // Set token expire time
-    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+  // Set token expire time
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
 
-    return resetToken;
-}
+  return resetToken;
+};
 
-export default mongoose.models.User || mongoose.model<IUser>('User', userSchema);
+export default mongoose.models.User ||
+  mongoose.model<IUser>("User", userSchema);
